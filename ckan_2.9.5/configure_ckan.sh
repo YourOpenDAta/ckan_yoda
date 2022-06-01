328f71e2247db8635a3fcccbee8e3e2ea41c6744
javicond3 <4b3javi@gmail.com>
2022-06-01 23:47:49 +0200

    add ckanext-yota_theme extension

--- ckan_2.9.5/configure_ckan.sh
+++ ckan_2.9.5/configure_ckan.sh
@@ -12,10 +12,16 @@ docker exec ckan_yoda /usr/local/bin/ckan -c /etc/ckan/production.ini datastore 
 docker exec -it ckan_yoda /usr/local/bin/ckan -c /etc/ckan/production.ini sysadmin add $ADMIN_USER_CKAN email=$ADMIN_USER_CKAN_EMAIL name=$ADMIN_USER_CKAN_NAME password=$ADMIN_USER_CKAN_PASS
 
 # update production.ini
-docker exec -it ckan_yoda /usr/local/bin/ckan config-tool /etc/ckan/production.ini -s  app:main 'ckan.plugins= stats text_view image_view recline_view datastore dcat dcat_rdf_harvester dcat_json_harvester dcat_json_interface structured_data harvest ckan_harvester oaipmh_edp' 'ckanext.dcat.rdf.profiles = euro_dcat_ap edp_dcat_ap' 'ckanext.dcat.base_uri = http://example.com' 'ckan.harvest.mq.type = redis'
+docker exec -it ckan_yoda /usr/local/bin/ckan config-tool /etc/ckan/production.ini -s  app:main 'ckan.plugins= stats text_view image_view recline_view datastore dcat dcat_rdf_harvester dcat_json_harvester dcat_json_interface structured_data harvest ckan_harvester oaipmh_edp yoda_theme' 'ckanext.dcat.rdf.profiles = euro_dcat_ap edp_dcat_ap' 'ckanext.dcat.base_uri = http://example.com' 'ckan.harvest.mq.type = redis'
 
 # configure ckanext-harvest extension
 docker exec -it ckan_yoda /usr/local/bin/ckan -c /etc/ckan/production.ini harvester initdb
+
+# configure ckan front-end settings
+docker exec -it ckan_yoda /usr/local/bin/ckan config-tool /etc/ckan/production.ini -s  app:main 'ckan.site_title = YODA' 'ckan.site_logo = /images/logo_yoda.png' 'ckan.favicon = /images/favicon_yoda.png' 'ckan.site_description = Portal de Datos en Abierto del Proyecto YODA (Your Open DAta)'
+
+# configure ckan internationalisation Settings
+docker exec -it ckan_yoda /usr/local/bin/ckan config-tool /etc/ckan/production.ini -s  app:main 'ckan.locale_default = es'
 
 
 docker compose -f ./docker-compose.yml restart ckan
