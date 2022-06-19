cfe5e6d02673075059e8c1db2add936b1cfdca47
javicond3 <4b3javi@gmail.com>
2022-06-19 22:05:51 +0200

    change edp_dcat_ap profile to dcat_ap_2.0.1

--- ckan_2.9.5/configure_ckan.sh
+++ ckan_2.9.5/configure_ckan.sh
@@ -12,7 +12,7 @@ docker exec ckan_yoda /usr/local/bin/ckan -c /etc/ckan/production.ini datastore 
 docker exec -it ckan_yoda /usr/local/bin/ckan -c /etc/ckan/production.ini sysadmin add $ADMIN_USER_CKAN email=$ADMIN_USER_CKAN_EMAIL name=$ADMIN_USER_CKAN_NAME password=$ADMIN_USER_CKAN_PASS
 
 # update production.ini
-docker exec -it ckan_yoda /usr/local/bin/ckan config-tool /etc/ckan/production.ini -s  app:main 'ckan.plugins= stats text_view image_view recline_view datastore dcat dcat_rdf_harvester dcat_json_harvester dcat_json_interface structured_data harvest ckan_harvester oaipmh_edp yoda_theme' 'ckanext.dcat.rdf.profiles = euro_dcat_ap edp_dcat_ap' 'ckanext.dcat.base_uri = http://example.com' 'ckan.harvest.mq.type = redis'
+docker exec -it ckan_yoda /usr/local/bin/ckan config-tool /etc/ckan/production.ini -s  app:main 'ckan.plugins= stats text_view image_view recline_view datastore dcat dcat_rdf_harvester dcat_json_harvester dcat_json_interface structured_data harvest ckan_harvester oaipmh_edp yoda_theme' 'ckanext.dcat.rdf.profiles = euro_dcat_ap dcat_ap_2.0.1' 'ckanext.dcat.base_uri = http://example.com' 'ckan.harvest.mq.type = redis'
 
 # configure ckanext-harvest extension
 docker exec -it ckan_yoda /usr/local/bin/ckan -c /etc/ckan/production.ini harvester initdb
