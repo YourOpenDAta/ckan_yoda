fe47eb89e7a364dca67a28db3bf96682205c1921
javicond3 <4b3javi@gmail.com>
2022-07-14 22:42:22 +0200

    update configuration for development

--- ckan_2.9.5/configure_ckan.sh
+++ ckan_2.9.5/configure_ckan.sh
@@ -11,12 +11,17 @@ docker exec ckan_yoda /usr/local/bin/ckan -c /etc/ckan/production.ini datastore 
 # create sysadmin account
 docker exec -it ckan_yoda /usr/local/bin/ckan -c /etc/ckan/production.ini sysadmin add $ADMIN_USER_CKAN email=$ADMIN_USER_CKAN_EMAIL name=$ADMIN_USER_CKAN_NAME password=$ADMIN_USER_CKAN_PASS
 
-# update production.ini
-docker exec -it ckan_yoda /usr/local/bin/ckan config-tool /etc/ckan/production.ini -s  app:main 'ckan.plugins= stats text_view image_view recline_view datastore dcat dcat_rdf_harvester dcat_json_harvester dcat_json_interface structured_data harvest ckan_harvester oaipmh_edp yoda_theme' 'ckanext.dcat.rdf.profiles = euro_dcat_ap dcat_ap_2.0.1' 'ckan.harvest.mq.type = redis'
-docker exec -it ckan_yoda /usr/local/bin/ckan config-tool /etc/ckan/production.ini -s  app:main ckanext.dcat.base_uri=$CKAN_SITE_URL
+# extensions
+docker exec -it ckan_yoda /usr/local/bin/ckan config-tool /etc/ckan/production.ini -s  app:main 'ckan.plugins= stats text_view image_view recline_view datastore'
 
-# configure ckanext-harvest extension
-docker exec -it ckan_yoda /usr/local/bin/ckan -c /etc/ckan/production.ini harvester initdb
+
+if [[ ${TARGET} != "dev" ]]; then
+    #production extensions
+    docker exec -it ckan_yoda /usr/local/bin/ckan config-tool /etc/ckan/production.ini -s  app:main 'ckan.plugins= stats text_view image_view recline_view datastore dcat dcat_rdf_harvester dcat_json_harvester dcat_json_interface structured_data harvest ckan_harvester oaipmh_edp yoda_theme' 'ckanext.dcat.rdf.profiles = euro_dcat_ap dcat_ap_2.0.1' 'ckan.harvest.mq.type = redis'
+    docker exec -it ckan_yoda /usr/local/bin/ckan config-tool /etc/ckan/production.ini -s  app:main ckanext.dcat.base_uri=$CKAN_SITE_URL
+    # configure ckanext-harvest extension
+    docker exec -it ckan_yoda /usr/local/bin/ckan -c /etc/ckan/production.ini harvester initdb
+fi
 
 # configure ckan front-end settings
 docker exec -it ckan_yoda /usr/local/bin/ckan config-tool /etc/ckan/production.ini -s  app:main 'ckan.site_title = YODA' 'ckan.site_logo = /images/logo_yoda.png' 'ckan.favicon = /images/favicon_yoda.png' 'ckan.site_description = Portal de Datos en Abierto del Proyecto YODA (Your Open DAta)'
